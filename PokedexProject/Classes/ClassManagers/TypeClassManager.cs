2f9590a9f5cb3f5305ef6c36b906ce86684b884f
lukemols <luca92mollo@hotmail.it>
2015-11-27 21:02:04 +0100

    Aggiunte classi TypeClassManager e GenerationClass

---
+++ PokedexProject/Classes/ClassManagers/TypeClassManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokedexProject
+{
+    static class TypeClassManager
+    {
+        private static List<Type> listaTipo;
+        static public List<Type> ListaTipo { get { return listaTipo; } }
+        
+        /// <summary>
+        /// Metodo che crea la lista dei tipi a partire da una lista di stringhe
+        /// </summary>
+        /// <param name="lines">Lista di stringhe contenenti i tipi</param>
+        static public void GetTipoList(List<string> lines)
+        {
+            listaTipo = new List<Type>();
+            
+            while (lines.Count > 0)
+            {
+                try
+                {
+                    Type t = new Type(lines[0]);
+                    listaTipo.Add(t);
+                }
+                catch (ProgramException) { }
+
+                lines.RemoveAt(0);
+            }
+        }
+
+        /// <summary>
+        /// Metodo che ritorna i tipi del Pokémon selezionato
+        /// </summary>
+        /// <param name="index">Indice del Pokémon</param>
+        /// <returns>Lista dei tipi</returns>
+        static public List<Type> GetPokeTypes(int index)
+        {
+            List<Type> types = new List<Type>();
+            foreach (Type t in listaTipo)
+                if (t.PokemonNumber == index)
+                    types.Add(t);
+            return types;
+        }
+    
+    }
+}
---
+++ PokedexProject/Classes/Program/GenerationClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokedexProject
+{
+    static class GenerationClass
+    {
+        static int[] generationLimit = { 0, 151, 251, 386, 493, 649, 721 };
+        static public int[] GenerationLimit { get { return generationLimit; } }
+
+        static int actualGeneration = 6;
+        static public int ActualGeneration
+        {
+            get
+            {
+                return actualGeneration;
+            }
+            set
+            {
+                if (value < 7 && value > 0)
+                    actualGeneration = value;
+            }
+        }
+    }
+}
